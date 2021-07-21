74ff93c0056a251101ee44344048db20dd32faf0
Anna Vu <anna.h.vu9@gmail.com>
2021-07-20 22:36:14 -0500

    prepare

--- prepare.py
+++ prepare.py
@@ -2,14 +2,49 @@ import pandas as pd
 import numpy as np
 
 def prep_logs(df):
+    '''
+    prep_logs will take in the curriculum log data and prepare it for use by 
+    setting date to the index, and converting it to datetime format,
+    changing start date and end date to datetime format,
+    renaming columns for easier use, drop unecessary columns, create new features,
+    create objectified columns for easier read, and drops duplicates
+    '''
+    #set our index to datetime format
     df.index = pd.to_datetime(df.index)
+    
+    #change start_date object column to datetime format
     df.start_date = pd.to_datetime(df.start_date)
+    
+    #change end_date object column to datetime format
     df.end_date = pd.to_datetime(df.end_date)
+    
+    #rename path to endpoint, and name to cohort
     df = df.rename(columns={'path':'endpoint',
                        'name':'cohort'})
+    
+    #drop id, as cohort_id is synonomous 
     df = df.drop(columns=['id'])
+    
+    #create accessed_after column which tells us if the user accessed the material after their graduation date at the time of query
     df['accessed_after'] = df.index > df.end_date
+    
+    #turn accessed_after to integers instead of booleans
     df['accessed_after'] = df['accessed_after'].astype(int)
+    
+    #create program_name column
+    df['program_name'] = 0
+    
+    #program_id of 1 is Full Stack PHP
+    df['program_name'] = np.where(df['program_id']==1, 'Full Stack PHP', 0)
+    
+    #program_id of 2 is Web Development
+    df['program_name'] = np.where(df['program_id']==2, 'Web Development', df['program_name'])
+    
+    #program_id of 3 is Data Science
+    df['program_name'] = np.where(df['program_id']==3, 'Data Science', df['program_name'])
+    
+    #program_id of 4 is Front End
+    df['program_name'] = np.where(df['program_id']==4, 'Front End', df['program_name'])
     
     # check for duplicates 
     num_dups = df.duplicated().sum()
