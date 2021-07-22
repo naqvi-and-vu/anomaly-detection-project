5b3febb87c1f7a26f68be103b917e9bc2c01e2f8
Anna Vu <anna.h.vu9@gmail.com>
2021-07-22 12:06:42 -0500

    acquire

--- acquire.py
+++ acquire.py
@@ -9,23 +9,6 @@ def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 
-# def new_curriculum_logs_data():
-#     '''
-#     Returns curriculum logs into a dataframe
-#     '''
-#     sql_query = '''select * from logs join cohorts on cohorts.id=logs.cohort_id'''
-#     df = pd.read_sql(sql_query, get_connection('curriculum_logs'))
-#     return df 
-
-# def get_curriculum_logs_data():
-#     '''get connection, returns curriculum log's logs table into a dataframe and creates a csv for us'''
-#     if os.path.isfile('curriculum-access.csv'):
-#         df = pd.read_csv('curriculum-access.csv', index_col=0)
-#     else:
-#         df = new_curriculum_logs_data()
-#         df.to_csv('curriculum-access.csv')
-#     return df
-
 def get_curriculum_data(cached=False):
     '''
     This function returns the curriculum logs database as a pandas dataframe. 
